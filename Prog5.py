6d107cf65c423a7aad10bc4e80fe00ad20cca1df
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-03-02 17:06:42 +0800

    Update Prog5.py

--- Prog5.py
+++ Prog5.py
@@ -1,5 +1,9 @@
 # Program: QuotientOfTwoNumbers.py
-n1 = float(input("Number 1: "))
-n2 = float(input("Number 2: "))
-result = n1 / n2
-print (result)
+n1 = float(input("Enter number 1: "))
+n2 = float(input("Enter number 2: "))
+
+if n2 == 0:
+    print ("Indeterminate")
+else:
+    result = n1/n2
+    print (f"Result: {n1 / n2:.2f}")

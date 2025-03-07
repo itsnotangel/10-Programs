5dec8633bd9dba6a55f9b6a94f00b18cc6c1dd0e
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-03-07 22:59:28 +0800

    Update Prog8.py

--- Prog8.py
+++ Prog8.py
@@ -1,8 +1,10 @@
+# Program 8: All Odd Numbers Total
+
 odd = 0
 
 for i in range(10):
-    num = float(input("Enter number " + str(i+1) + ": "))
-    if i % 2 != 0:
+    num = int(input("Enter number " + str(i+1) + ": "))
+    if num % 2 != 0:
         odd += 1
 
 print (odd)
